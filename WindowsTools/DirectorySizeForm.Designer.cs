6b4514241ed19a6039ce64eee3601f723ef4c59f
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2018-05-08 10:27:44 +0300

    Working on Directory Sizes.

---
+++ WindowsTools/DirectorySizeForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsTools
+{
+    partial class DirectorySizeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenPath = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(12, 25);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(393, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Directory:";
+            // 
+            // btnOpenPath
+            // 
+            this.btnOpenPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenPath.Location = new System.Drawing.Point(411, 23);
+            this.btnOpenPath.Name = "btnOpenPath";
+            this.btnOpenPath.Size = new System.Drawing.Size(32, 23);
+            this.btnOpenPath.TabIndex = 2;
+            this.btnOpenPath.Text = "...";
+            this.btnOpenPath.UseVisualStyleBackColor = true;
+            this.btnOpenPath.Click += new System.EventHandler(this.btnOpenPath_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(12, 51);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.Location = new System.Drawing.Point(12, 80);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(431, 253);
+            this.treeView1.TabIndex = 4;
+            // 
+            // DirectorySizeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 345);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnOpenPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Name = "DirectorySizeForm";
+            this.Text = "Directory Sizes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenPath;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ WindowsTools/DirectorySizeForm.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsTools
+{
+    public partial class DirectorySizeForm : Form
+    {
+        public string m_Path { get; set; } = String.Empty;
+
+        public DirectorySizeForm()
+        {
+            InitializeComponent();
+
+            treeView1.ImageList = new ImageList();
+            treeView1.ImageList.Images.Add(WindowsTools.Properties.Resources.folder16x16);
+            treeView1.ImageList.Images.Add(WindowsTools.Properties.Resources.file16x16);
+            treeView1.ImageList.Images.Add(WindowsTools.Properties.Resources.foldererror16x16);
+            treeView1.ImageList.Images.Add(WindowsTools.Properties.Resources.fileerror16x16);
+        }
+
+        #region Event Handlers
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            BuildDirectoryTree();
+        }
+
+        private void btnOpenPath_Click(object sender, EventArgs e)
+        {
+            var dlg = new FolderBrowserDialog();
+            dlg.SelectedPath = m_Path;
+
+            var result = dlg.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                txtPath.Text = dlg.SelectedPath;
+                m_Path = dlg.SelectedPath;
+            }
+        }
+
+        #endregion
+
+
+        #region Helper Methods
+
+        private void BuildDirectoryTree()
+        {
+            treeView1.Nodes.Clear();
+
+            string p = String.Empty;
+
+            try
+            {
+                p = Path.GetFullPath(txtPath.Text.Replace("\"", "")) + "\\";
+                p = p.Replace("\\\\", "\\");
+            }
+            catch { }
+
+            if (!Directory.Exists(p))
+            {
+                MessageBox.Show("The specified folder does not exists.");
+                return;
+            }
+
+            m_Path = p;
+
+            var node = new TreeNode(Path.GetDirectoryName(m_Path));
+            treeView1.Nodes.Add(node);
+
+            var start = DateTime.Now;
+
+            var size = EnumerateSubdirs(m_Path, treeView1.Nodes[0]);
+            node.Text = FormatSize(size) + " : " + node.Text;
+
+            var end = DateTime.Now;
+
+            var duration = end - start;
+
+            MessageBox.Show("Processed in " + duration.TotalMinutes + "minutes.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private long EnumerateSubdirs(string path, TreeNode node)
+        {
+            long size = 0;
+
+            try
+            {
+                var directories = Directory.EnumerateDirectories(path);
+
+                foreach (var d in directories)
+                {
+                    //node.Nodes.Add(d);
+                    var tn = new TreeNode();
+                    node.Nodes.Add(tn);
+
+                    var sizeSubdir = EnumerateSubdirs(d, tn);
+                    size += sizeSubdir;
+
+                    //node.Nodes.Add(Path.GetFileName(d));
+                    tn.Text = FormatSize(sizeSubdir) + " : " + Path.GetFileName(d);
+                }
+
+                var files = Directory.EnumerateFiles(path);
+
+                foreach (var f in files)
+                {
+                    var sizeFile = new FileInfo(f).Length;
+                    var n = new TreeNode(FormatSize(sizeFile) + " : " + Path.GetFileName(f));
+                    n.ImageIndex = 1;
+                    n.SelectedImageIndex = 1;
+                    node.Nodes.Add(n);
+
+                    size += sizeFile;
+                }
+            }
+            catch
+            {
+                // var tn = new TreeNode("              ? : " + Path.GetFileName(path));
+                // tn.ImageIndex = 2;
+                // tn.SelectedImageIndex = 2;
+                //node.Nodes.Add(tn);
+                node.ImageIndex = 2;
+                node.SelectedImageIndex = 2;
+            }
+
+            return size;
+        }
+
+        private string FormatSize(long size)
+        {
+            return String.Format("{0:N0}", size).PadLeft(15);
+        }
+
+        #endregion
+    }
+}
