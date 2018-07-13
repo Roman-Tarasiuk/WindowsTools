b8df0c9401cb2d0153c40ed1be3d47f605be8a61
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2018-07-13 10:43:02 +0300

    Screen Ruler Panel: Changed initial size.

--- WindowsTools/ScreenRulerArrangePanelForm.Designer.cs
+++ WindowsTools/ScreenRulerArrangePanelForm.Designer.cs
@@ -104,7 +104,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(184, 161);
+            this.ClientSize = new System.Drawing.Size(160, 110);
             this.ContextMenuStrip = this.contextMenuStrip1;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
