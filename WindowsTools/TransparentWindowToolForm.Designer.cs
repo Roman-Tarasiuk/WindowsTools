a92ebf40ceaea8bdeeb67b7f3452151010d4c761
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2018-08-07 19:41:43 +0300

    Set window transparency tool: Changed taskbar icon.

--- WindowsTools/TransparentWindowToolForm.Designer.cs
+++ WindowsTools/TransparentWindowToolForm.Designer.cs
@@ -36,10 +36,10 @@ namespace WindowsTools
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.resetTransparencyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.topmostToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.showInTaskbarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -66,23 +66,16 @@ namespace WindowsTools
             // resetTransparencyToolStripMenuItem
             // 
             this.resetTransparencyToolStripMenuItem.Name = "resetTransparencyToolStripMenuItem";
-            this.resetTransparencyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resetTransparencyToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
             this.resetTransparencyToolStripMenuItem.Text = "Reset transparency";
             this.resetTransparencyToolStripMenuItem.Click += new System.EventHandler(this.resetTransparencyToolStripMenuItem_Click);
-            // 
-            // closeToolStripMenuItem
-            // 
-            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.closeToolStripMenuItem.Text = "Close";
-            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
             // 
             // topmostToolStripMenuItem
             // 
             this.topmostToolStripMenuItem.Checked = true;
             this.topmostToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
             this.topmostToolStripMenuItem.Name = "topmostToolStripMenuItem";
-            this.topmostToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.topmostToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
             this.topmostToolStripMenuItem.Text = "Topmost";
             this.topmostToolStripMenuItem.Click += new System.EventHandler(this.topmostToolStripMenuItem_Click);
             // 
@@ -91,14 +84,21 @@ namespace WindowsTools
             this.showInTaskbarToolStripMenuItem.Checked = true;
             this.showInTaskbarToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
             this.showInTaskbarToolStripMenuItem.Name = "showInTaskbarToolStripMenuItem";
-            this.showInTaskbarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.showInTaskbarToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
             this.showInTaskbarToolStripMenuItem.Text = "Show in taskbar";
             this.showInTaskbarToolStripMenuItem.Click += new System.EventHandler(this.showInTaskbarToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(170, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
             // 
             // TransparentWindowToolForm
             // 
